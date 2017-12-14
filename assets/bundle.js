2febc93e781ad68628a8f22df5f0c128ada085ee
TheoCouture <theo.couture@gmail.com>
2017-12-14 13:39:33 +0100

    Tableau ok

--- assets/bundle.js
+++ assets/bundle.js
@@ -3,65 +3,79 @@ var HexGrid = require('../assets/hex-grid');
 
 var invalidPos = ['0.0', '1.0.5', '2.0', '4.0', '5.0.5', '6.0', '7.0.5', '8.0', '0.1', '1.1.5', '8.1', '0.2', '8.2', '8.3', '0.5', '0.6', '7.6.5', '8.6', '0.7', '1.7.5', '3.7.5', '7.7.5', '8.7']
 
-var pieces = {
-    'VIDE': {
-        id: 0,
-		src: 'src',
-		nb: 7
-    }, 
-    'BLEU': {
+var pieces = [
+    {
+        name : 'BLEU',
         id: 1,
-		src: 'src',
+		src: './img/blue.png',
 		nb: 7
     },
-    'JAUNE': {
+    {
+        name : 'JAUNE',
         id: 2,
-		src: 'src',
-		nb: 7
+        src: './img/yellow.png',
+        nb: 7
     },
-    'ROUGE': {
+    {
+        name : 'ROUGE',
         id: 3,
-		src:'src',
+		src:'./img/red.png',
 		nb: 7
 	},
-	'VERT': {
+	{
+        name : 'VERT',
 		id: 4,
-		src: 'src',
+		src: './img/green.png',
 		nb: 7
 	},
-	'VIOLET': {
+	{
+		name : 'VIOLET',
 		id: 5,
-		src: 'src',
+		src: './img/purple.png',
 		nb: 7
 	},
-	'ORANGE': {
+	{
+        name : 'ORANGE',
 		id: 6,
-		src: 'src',
+		src: './img/orange.png',
 		nb: 7
 	},
-	'BLANC': {
+	{
+        name : 'BLANC',
 		id: 7,
-		src: 'src',
+		src: './img/white.png',
 		nb: 7
 	}
-};
+];
 
 var colours = [];
 
-App.prototype.initColors = function () {
+var initColors = function () {
 	pieces.forEach(function (piece) {
-		console.log(piece);
-	})
+
+		for (var i = 0; i< 7; i++){
+            colours.push(piece);
+		}
+
+	});
+
+    var j, x, i;
+    for (i = colours.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = colours[i];
+        colours[i] = colours[j];
+        colours[j] = x;
+    }
 }
 
 var TileFactory = function () {
 	var _id = 0;
 	return {
 		newTile: function () {
+			console.log(_id);
 			var tile = {
 				id: _id.toString(),
-				type: 'testTile',
-				color: 'MACOULEUR'
+				type: 'testTile'
 			};
 
 			_id += 1;
@@ -179,18 +193,24 @@ function App(options) {
 		tileSize: this.tileSize
 	});
 
+	initColors();
+
 	var iter = this.hexGrid.getTileIterator();
 	var tile = iter.next();
-	var tilePos, pos;
+	var tilePos, pos, i = 0;
 	while (tile !== null) {
 		tilePos = this.hexGrid.getPositionById(tile.id);
 		pos = tilePos.x + '.' + tilePos.y;
 		if (!invalidPos.includes(pos)){
 			tile.element = this.dtd.createDomTile(tilePos.x, tilePos.y);
+			console.log(tile)
+            tile.color = colours[i];
 			this.dtd.setTileImage(
 				tile.element,
-				this.getTileImageByPos(tilePos.x, tilePos.y)
+				tile.color.src
 			);
+			i++;
+			console.log(this.tile);
 		}
 		tile = iter.next();
 	}
