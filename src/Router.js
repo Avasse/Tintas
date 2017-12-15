78932657b5402a23b345236f548f12f0bd41c84a
Avasse <avasse@partner.auchan.fr>
2017-12-15 14:18:07 +0100

    Fix : Graphicals imp

--- src/Router.js
+++ src/Router.js
@@ -20,7 +20,9 @@ router.on({
     // 'view' is the id of the div element inside which we render the HTML
     'firstroute': () => { loadHTML('/src/templates/home.html', 'view') },
     'secondroute': () => { loadHTML('/src/templates/rules.html', 'view') },
-    'thirdroute': () => { loadHTML('/src/templates/grid.html', 'view') }
+    'thirdroute': () => { 
+        loadHTML('/src/templates/grid.html', 'view');
+    }
 });
 
 // set the default route
